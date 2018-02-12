746fa067975df898eb82650968b7cc75f6a7b661
markonni93 <marko.djurdjev93@gmail.com>
2018-02-12 12:06:04 +0100

    doda kod za resenje

--- 11172 - Relational Operator/Main.java
+++ 11172 - Relational Operator/Main.java
@@ -4,13 +4,27 @@ import java.util.Scanner;
 
 
 public class Main {
-  public static void main(String args[]) {
+public static void main(String args[]) {
 
 
-      Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
-      
+        int t = in.nextInt();
 
 
-  }
+        for (int i = 0; i < t; i++) {
+                int prvibroj = in.nextInt();
+                int drugibroj = in.nextInt();
+
+                if (prvibroj < drugibroj) {
+                        System.out.println("<");
+                } else if (prvibroj == drugibroj) {
+                        System.out.println("=");
+                } else{
+                        System.out.println(">");
+                }
+        }
+
+
+}
 }
