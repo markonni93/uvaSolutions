44ee1d7a7789ec5bec32d1ff207d1b782c094b32
Aleksandar Markovic <aleksamarkoni@gmail.com>
2018-02-17 00:16:49 +0100

    Predolog za promene
    Cao mare sve je super, ali eto samo sam hteo da prodiskutujemo sta sve jos moze. Pozz.

--- 12372 - Packing for Holiday/Main.java
+++ 12372 - Packing for Holiday/Main.java
@@ -16,18 +16,25 @@ public class Main {
     int printCounter = 0;
     String print = "";
 
-
+//ovde moze slobodno i ovo while(counter++ < numCase)
     while(counter < numCase){
-
+//TODO ovde ti fale da su ove linije uvucene za po jos jedno tab mesto
     L = in.nextInt();
     W = in.nextInt();
     H = in.nextInt();
 
+      //ovo je super, osim sto ti ne treba ova provera u else, mislim ako je ovo prvo dobro, onda jedino sto ti preostane jeste da nije
     if (L <= 20 && W <= 20 && H <= 20) {
         print = "good";
-    } else if (L > 20 || W > 20 || H > 20) {
+    } else if (L > 20 || W > 20 || H > 20) { // tako da ti ovo ovde if ne treba, treba samo else
         print = "bad";
     }
+      //cisto radi vezbe probaj da uradis gornji if i kao ternary operator https://alvinalexander.com/java/edu/pj/pj010018
+      //nesto tipa ovako 
+      //print = (uslov) ? jeste : nije;
+      
+      
+      // i ovde print counter moze da se sredi i ubaci u system.out.println, ili da se koristi System.out.printf
      printCounter++;
      System.out.println("Case " + printCounter + ": " + print);
      counter++;
